49d9c1463e4429df80d9b81d080062d8378adb8a
unknown <asazycat@gmail.com>
2025-11-21 19:34:29 +0000

    login screen

--- src/app/Login/LoginServer.tsx
+++ src/app/Login/LoginServer.tsx
@@ -1,16 +1,23 @@
 
 
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Box, FormGroup } from "@mui/material";
 import { credentialsAction } from "../actions";
 
 
 export default async function LoginServer() {
  
        return (
-          <form action={credentialsAction}>
-            <TextField required name="email" label="Email" type="email" placeholder="email" aria-required defaultValue={'ali@gmail.com'}/>
-            <TextField name="password" label="Password" type="password" placeholder="password" autoComplete="current-password" defaultValue={'ali'} required/>
-            <Button type="submit" variant="contained" color="primary">Login</Button>
-          </form>
+        <Box sx={{display:"flex", flexDirection: "column", margin: "auto", height: "100vh", justifyContent: "center", textAlign:"center"}}>
+            <form action={credentialsAction}>
+              <FormGroup sx={{width:'50%', margin:"auto", backgroundColor: 'lightblue', padding:"2em", borderRadius:"20px"}}>
+                <TextField required name="email" label="Email" type="email" placeholder="email" aria-required defaultValue={'ali@gmail.com'} sx={{marginTop:"2em", backgroundColor: 'white', borderRadius:"10px"}}/>
+                <TextField name="password" label="Password" type="password" placeholder="password" autoComplete="current-password" defaultValue={'ali'} required sx={{marginTop:"2em", backgroundColor: 'white', borderRadius:"10px"}}/>
+                <FormGroup sx={{display:"flex", flexDirection: "row"}}>
+                  <Button type="submit" variant="contained" color="primary" sx={{width:'25%', margin:"auto", marginTop:"2em"}}>Login</Button>
+                  <Button type="submit" variant="contained" color="primary" sx={{width:'25%', margin:"auto", marginTop:"2em"}}>Demo</Button>
+                </FormGroup>
+              </FormGroup>
+            </form>
+        </Box>
          );
  }
