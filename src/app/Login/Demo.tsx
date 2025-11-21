4d861b03493493187c312f9d06a484ad9186b6ba
unknown <asazycat@gmail.com>
2025-11-21 21:13:07 +0000

    demo button working

---
+++ src/app/Login/Demo.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Button } from "@mui/material"
+import { credentialsAction } from "../actions"
+
+export default function Demo() {
+        const demoFormData = new FormData(); 
+          demoFormData.append("email", 'ali@gmail.com'); 
+          demoFormData.append("password", 'ali'); 
+
+          return (
+    <>
+        <Button type="button" onClick={() => { credentialsAction(demoFormData)}} variant="contained" color="primary" sx={{width:'25%', margin:"auto", marginTop:"2em"}}>Demo</Button>
+    </>
+          )
+}
